4af7fecf14d3b8f332787c4e2df85974cd05c775
Meenu parashar <meenuparashar18@gmail.com>
2026-06-18 20:25:08 +0530

    patterns

---
+++ Time complexity and Pattern/pattern.cpp
@@ -0,0 +1,21 @@
+//Ques1--
+/*
+****
+****
+****
+****
+*/
+
+#include<bits/stdtr1c++.h>
+using namespace std;
+void print1(){
+    for(int i =0; i<4; i++){
+        for(int j=0; j<4; j++){
+            cout <<"* ";
+        }
+        cout <<endl;
+    }
+}
+    int main(){
+        print1();
+    }
