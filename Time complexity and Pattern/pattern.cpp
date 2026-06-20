5b5a91a6c2cc5d7c6067fde48b26eb1ccf46f0a2
Meenu parashar <meenuparashar18@gmail.com>
2026-06-20 18:32:50 +0530

    pattern9

--- Time complexity and Pattern/pattern.cpp
+++ Time complexity and Pattern/pattern.cpp
@@ -328,10 +328,66 @@ using namespace std;
 // }
 // cout <<endl;
 //     }
-// //
 //     }
 // int main(){
 //     int n;
 //     cin >>n;
 //     print8(n);
 // }
+
+//QUES9-------------------------------------
+/*
+*
+***
+*****
+*******
+*********
+*********
+*******
+*****
+***
+*
+//Apporach its combine pattern of 8 and 9 so merge the both coded 
+
+*/
+
+//normal
+void print7(int n){
+    for(int i=0; i<n; i++){
+        //space
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+        //star
+        for(int j=0; j<2*i+1; j++){
+            cout << "*";
+        }
+        //space
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+        cout <<endl;
+
+    }
+}
+void print8(int n){
+    for(int i=0; i<n; i++){
+//space
+for(int j=0; j<i; j++){
+    cout<< " ";
+}
+//star
+for(int j=0; j<2*n-(2*i+1); j++){
+    cout << "*";
+}
+for(int j=0; j<i; j++){
+    cout<< " ";
+}
+cout <<endl;
+    }
+//
+    }
+    int main(){
+        print7(5);
+         print8(5);
+    }
