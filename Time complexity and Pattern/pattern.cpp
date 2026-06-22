cd9ef17e528518052f5065c43af033d347128a38
Meenu parashar <meenuparashar18@gmail.com>
2026-06-22 19:29:02 +0530

    pattern10

--- Time complexity and Pattern/pattern.cpp
+++ Time complexity and Pattern/pattern.cpp
@@ -393,7 +393,7 @@ using namespace std;
 //     }
 
 
-//Ques-9--------------------------------
+//Ques-10--------------------------------
 /*
 *
 **
@@ -406,20 +406,21 @@ using namespace std;
 *
 */
 void print10(int n){
-    for(int i=0; i<n; i++){
-       for(int j=0; j<i; j++){
-        cout << " ";
-       } 
-       for(int j=0; j<2*n-1; j++){
-        cout << "*";
-       }
-       for(int j=0; j<i; j++){
-        cout << " ";
-       }
-       cout << endl;
+    for(int i=1; i<=2*n-1; i++){
+     int stars = i;
+     if(i >n ) stars = 2*n - i;
+     for(int j=1; j<=stars; j++){
+        cout<< "*";
+     }
+     cout << endl;
     }
-
 }
-int main(){
-    print10(9);
+int main() {
+ int t;
+ cin >> t;
+ for(int i=0; i<t; i++){
+    int n;
+    cin >> n;
+    print10(n);
+ }
 }
