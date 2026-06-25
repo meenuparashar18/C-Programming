be7859ec0b272bf9b6d524eead5c3fb01d7d21b1
Meenu parashar <meenuparashar18@gmail.com>
2026-06-25 08:28:43 +0530

    pattern19

--- Time complexity and Pattern/pattern.cpp
+++ Time complexity and Pattern/pattern.cpp
@@ -624,15 +624,77 @@ BCDE
 ABCDE
 */
 
-void print18(int n){
-    for(int i=0; i<n; i++){
-for(char ch= 'E' -i; ch <= 'E'; ch++){
-    cout << ch <<" ";
-}
-cout << endl;
-    } 
+// void print18(int n){
+//     for(int i=0; i<n; i++){
+// for(char ch= 'E' -i; ch <= 'E'; ch++){
+//     cout << ch <<" ";
+// }
+// cout << endl;
+//     } 
 
+// }
+// int main(){
+//     print18(5);
+// }
+
+
+//Ques19-------------------------------------------------------------------
+/*
+**********
+****  ****
+***    ***
+**      **
+*       *
+*       *
+**      **
+***     ***
+****    ****
+**********
+*/
+//stars
+//space
+//star
+void print19(int n){
+    //upper 
+    int inis = 0;
+    for(int i =0; i<n; i++){
+    //star
+    for(int j=1; j<=n-i; j++){
+        cout << "*";
+    }
+    //space
+    for(int j=0; j<inis; j++){
+        cout << " ";
+    }
+    //star
+    for(int j=1; j<=n-i; j++){
+        cout << "*";
 }
+inis += 2;
+cout << endl;
+
+    }
+    //lower
+    inis = 8;
+    for(int i=0; i<=n; i++){
+           //star
+    for(int j=1; j<=i; j++){
+        cout << "*";
+    }
+    //space
+    for(int j=0; j<inis; j++){
+        cout << " ";
+    }
+    //star
+    for(int j=1; j<=i; j++){
+        cout << "*";
+}
+inis -= 2;
+cout << endl;
+    }
+}
+
 int main(){
-    print18(5);
+    print19(5);
 }
+
