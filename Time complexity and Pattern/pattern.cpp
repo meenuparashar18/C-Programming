5c491689da9a3d118c54170cf558e97a3d061198
Meenu parashar <meenuparashar18@gmail.com>
2026-06-25 09:17:48 +0530

    pattern21

--- Time complexity and Pattern/pattern.cpp
+++ Time complexity and Pattern/pattern.cpp
@@ -714,34 +714,63 @@ ABCDE
 */
 
 
-void print20(int n){
-    int spaces = 2*n-2;
-    for(int i=1; i<=2*n-1; i++){
-        int stars = i;
-        if(i > n) stars=2*n-i;
-        //stars
-        for(int j=1; j<=stars; j++){
-            cout << "*";
-        }
-        //spaces
-        for(int j=1; j<=spaces; j++){
-            cout << " ";
-        }
-        //stars
-         for(int j=1; j<=stars; j++){
-            cout << "*";
+// void print20(int n){
+//     int spaces = 2*n-2;
+//     for(int i=1; i<=2*n-1; i++){
+//         int stars = i;
+//         if(i > n) stars=2*n-i;
+//         //stars
+//         for(int j=1; j<=stars; j++){
+//             cout << "*";
+//         }
+//         //spaces
+//         for(int j=1; j<=spaces; j++){
+//             cout << " ";
+//         }
+//         //stars
+//          for(int j=1; j<=stars; j++){
+//             cout << "*";
+//         }
+//         cout << endl;
+//         if(i<n)spaces -=2;
+//         else spaces +=2;
+//     }
+// }
+// int main(){
+//  int t;
+//  cin >>t;
+//  for(int i=0; i<t; i++){
+//     int n; 
+//     cin >> n;
+//     print20(n);
+//  }
+// }
+
+//Ques21----------------------------------------------------------------------
+/*
+****
+*  *
+*  *
+****
+*/
+
+ void print21(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i==0 || j==0 || i==n-1 || j== n-1){
+                cout << "*";
+            }
+            else cout << " ";
         }
         cout << endl;
-        if(i<n)spaces -=2;
-        else spaces +=2;
     }
-}
-int main(){
- int t;
- cin >>t;
- for(int i=0; i<t; i++){
-    int n; 
-    cin >> n;
-    print20(n);
  }
-}
+ int main(){
+    int t;
+    cin >> t;
+    for(int i=0; i<t; i++){
+        int n;
+        cin >> n;
+        print21(n);
+    }
+ }
