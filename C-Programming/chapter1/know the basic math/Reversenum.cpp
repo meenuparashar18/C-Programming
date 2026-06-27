93253ef8c63349cbf63ac0fb328b71b69748e72b
Meenu parashar <meenuparashar18@gmail.com>
2026-06-27 20:28:46 +0530

    Reverse num

--- C-Programming/chapter1/know the basic math/Reversenum.cpp
+++ C-Programming/chapter1/know the basic math/Reversenum.cpp
@@ -1,17 +1,17 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int n = 0;
-    cin >> n;
-    int rev = 0;
-    while(n > 0){
-       int lastdigit = n%10;
-         rev =  rev*10+lastdigit;
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n = 0;
+//     cin >> n;
+//     int rev = 0;
+//     while(n > 0){
+//        int lastdigit = n%10;
+//          rev =  rev*10+lastdigit;
 
 
     
-    n = n/10;
-    }
-    cout << rev;
-    return 0;
-}
+//     n = n/10;
+//     }
+//     cout << rev;
+//     return 0;
+// }
