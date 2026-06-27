4aa3e392f5c4de779687a3c69a11d755784c67ab
Meenu parashar <meenuparashar18@gmail.com>
2026-06-27 20:29:29 +0530

    Palindrome

--- C-Programming/chapter1/know the basic math/Palindrome.cpp
+++ C-Programming/chapter1/know the basic math/Palindrome.cpp
@@ -1,23 +1,23 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int n =0;
-    cin >> n;
-    int rev = 0;
-    int orig = n;
-    while(n  > 0){
-        int lastdigit = n%10;
-         rev =  rev*10+lastdigit;
-        n = n/10;
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n =0;
+//     cin >> n;
+//     int rev = 0;
+//     int orig = n;
+//     while(n  > 0){
+//         int lastdigit = n%10;
+//          rev =  rev*10+lastdigit;
+//         n = n/10;
       
         
         
-    }
-    if(orig == rev) 
-    {
-        cout << "palindrome";
-    }else{
-        cout << "not";
-    }
-    return 0;
-}
+//     }
+//     if(orig == rev) 
+//     {
+//         cout << "palindrome";
+//     }else{
+//         cout << "not";
+//     }
+//     return 0;
+// }
