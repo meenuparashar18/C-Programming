bbcae5962983551938ed27e406e5b8cf4b08f17d
Meenu parashar <meenuparashar18@gmail.com>
2026-06-29 21:37:35 +0530

    bruth force prime number

--- C-Programming/chapter1/know the basic math/prime.cpp
+++ C-Programming/chapter1/know the basic math/prime.cpp
@@ -1,21 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+//reduce tc 
+// int main(){
+//     int n;
+//     cin >>n;
+//     int count = 0;
+//      for(int i =1; i*i<=n; i++){
+//         if(n%i ==0){
+//             count++;
+//             if((n/i)!=i){
+//                 count++;
+//             }
+//         }
+//      }
+//      if(count == 2){
+//         cout << "true";
+//      }else{
+//         cout << "false";
+//      }
+//     return 0;
+// }
+
+//bruth tc
 int main(){
-    int n;
-    cin >>n;
-    int count = 0;
-     for(int i =1; i*i<=n; i++){
-        if(n%i ==0){
+    int count =0;
+    int n=0;
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        if(n % i==0){
             count++;
-            if((n/i)!=i){
-                count++;
-            }
+
+
         }
-     }
-     if(count == 2){
-        cout << "true";
-     }else{
-        cout << "false";
-     }
+    }
+        if(count == 2){
+            cout << "true";
+        }else{
+           cout << "false";
+        }
+    
     return 0;
 }
