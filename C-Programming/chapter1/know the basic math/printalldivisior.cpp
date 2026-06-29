2ebf7f327078fbe972cccbde68cda670c73ffb61
Meenu parashar <meenuparashar18@gmail.com>
2026-06-29 21:28:31 +0530

    prime number

--- C-Programming/chapter1/know the basic math/printalldivisior.cpp
+++ C-Programming/chapter1/know the basic math/printalldivisior.cpp
@@ -1,5 +1,5 @@
-#include<bits/stdc++.h>
-using namespace std;
+// #include<bits/stdc++.h>
+// using namespace std;
 //time complexity is O(n);
 
 // void printdivisior(int n){
@@ -18,24 +18,24 @@ using namespace std;
 // }
 
 //2nd method reduce the time complexity
-void printdivisior(int n){
-    vector<int>ls;
-    for(int i=1; i<=sqrt(n); i++){
-        if(n%i == 0){
-            ls.push_back(i);
-        }
-        if((n/i)!=i){
-            ls.push_back(n/i);
-        }
-    }
+// void printdivisior(int n){
+//     vector<int>ls;
+//     for(int i=1; i<=sqrt(n); i++){
+//         if(n%i == 0){
+//             ls.push_back(i);
+//         }
+//         if((n/i)!=i){
+//             ls.push_back(n/i);
+//         }
+//     }
 
-//sorting the list
-sort(ls.begin(),ls.end());
-for(auto it : ls)
-cout << it << " ";
-}
-int main(){
-    int n;
-    cin >> n;
-    printdivisior(n);
-}
+// //sorting the list
+// sort(ls.begin(),ls.end());
+// for(auto it : ls)
+// cout << it << " ";
+// }
+// int main(){
+//     int n;
+//     cin >> n;
+//     printdivisior(n);
+//}
